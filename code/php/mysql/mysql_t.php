36bcde3d3168e41a04cc532a5663f1585d7ecea5
libo <libo@eyou.net>
2014-04-18 14:35:42 +0800

    增加 mysql 超时测试脚本

--- code/php/mysql/mysql_t.php
+++ code/php/mysql/mysql_t.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 
 $cmd = 'sudo -u eyou /usr/local/eyou/toolmail/opt/bin/php /home/libo/git/eagleeye/src/app/lib/monitor/operator/test/test_em_monitor_property_operator_user.php';
-$cmd = 'sudo -u eyou /usr/local/eyou/toolmail/opt/bin/php /home/libo/git/elephant_tk/src/lib/api/action/test/test_em_httpapi_action_api_user.class.php';
+//$cmd = 'sudo -u eyou /usr/local/eyou/toolmail/opt/bin/php /home/libo/git/elephant_tk/src/lib/api/action/test/test_em_httpapi_action_api_user.class.php';
 $i = 1;
 do {
     sleep(5*$i);
@@ -21,7 +21,7 @@ do {
         break;
     }
 
-    //++$i;
+    ++$i;
     echo "\n";
 
 } while (true);
--- code/php/mysql/mysql_t1.php
+++ code/php/mysql/mysql_t1.php
@@ -4,12 +4,13 @@ require_once PATH_EYOUM_LIB . 'em_db.class.php';
 
 $db = em_db::singleton();
 
-$select = "select * from user where email='libo@eyou.net'";
-$stmt = $db->query($select);
-//$db->check_connection();
-$res = $stmt->fetch_all();
-//print_r($stmt);
-//print_r($db->get_connection());
-print_r($res);
-//print_r($stmt->getAttribute(PDO::ATTR_DRIVER_NAME));
+    $select = "select * from user where email='libo@eyou.net'";
+    $select = "show processlist";
+    $stmt = $db->query($select);
+    //$db->check_connection();
+    $res = $stmt->fetch_all();
+    //print_r($stmt);
+    //print_r($db->get_connection());
+    print_r($res);
+    //print_r($stmt->getAttribute(PDO::ATTR_DRIVER_NAME));
 
