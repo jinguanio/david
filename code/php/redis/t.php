f898f0fbb0d3d1278f04c88a6fe2d3ba816908f9
libo <libo@eyou.net>
2014-05-06 16:56:48 +0800

    测试 php redis 操作，del key 和 hash del

--- code/php/redis/t.php
+++ code/php/redis/t.php
@@ -5,6 +5,25 @@ require_once EMBASE_PATH_EYOU_TOOLMAIL_CONF . 'conf_global.php';
 require_once PATH_EYOUM_LIB . 'em_redis.class.php';
 
 $redis = em_redis::connection_singleton();
+
+// del key
+$redis->set('key1', 'val1');
+echo "del key1: ";
+var_dump($redis->del('key1'));
+echo "\ndel key1: ";
+var_dump($redis->del('key1'));
+echo "\ndel key2: ";
+var_dump($redis->del('key2'));
+echo "\n";
+
+// hash del
+$redis->delete('h');
+$redis->delete('k');
+$redis->hSet('h', 'a', 'x');
+var_dump($redis->hDel('h', 'a'));
+var_dump($redis->hDel('k', 'c'));
+exit;
+
 //$redis->rPush('k1', 'v1');
 //$redis->rPush('k1', 'v2');
 //$redis->rPush('k1', 'v3');
