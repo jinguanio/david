5f548a4b8d722ffa523112f32a51850f5c6cec11
libo <libo@eyou.net>
2014-06-11 11:36:56 +0800

    处理 bash 多个变量放在一起避免混淆的事情

--- code/bash/esop/vopen.sh
+++ code/bash/esop/vopen.sh
@@ -19,10 +19,10 @@ fi
 git="/home/libo/git"
 api="$git/tk/src/lib/api/action"
 api_prefix="em_httpapi_action_api_"
-api_file="$api/$api_prefix$1.class.php"
+api_file="$api/${api_prefix}${1}.class.php"
 
 weblib="$git/eagleeye/src/app/lib/monitor/operator"
 weblib_prefix="em_monitor_property_operator_"
-weblib_file="$weblib/$weblib_prefix$1.class.php"
+weblib_file="$weblib/${weblib_prefix}${1}.class.php"
 
 vim -O $api_file $weblib_file
