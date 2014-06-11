d1674e223d1c41f0bbbd0bae7c646b1f8d0164f9
libo <libo@eyou.net>
2014-06-11 11:25:19 +0800

    添加打开 api 和 web lib 对应文件脚本

---
+++ code/bash/esop/vopen.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  vopen.sh
+# 
+#         USAGE:  ./vopen.sh 
+# 
+#   DESCRIPTION:  打开 web lib 和 api 对应文件
+# 
+#        AUTHOR:  jacob, libo@eyou.net
+#       CREATED:  2014年06月11日 11时17分04秒 CST
+#===============================================================================
+
+if [ -z $@ ]; then
+    echo "$0 [api lib suffix] [web lib suffix]"
+    exit 1;
+fi
+
+git="/home/libo/git"
+api="$git/tk/src/lib/api/action"
+api_prefix="em_httpapi_action_api_"
+api_file="$api/$api_prefix$1.class.php"
+
+weblib="$git/eagleeye/src/app/lib/monitor/operator"
+weblib_prefix="em_monitor_property_operator_"
+weblib_file="$weblib/$weblib_prefix$2.class.php"
+
+vim -O $api_file $weblib_file
