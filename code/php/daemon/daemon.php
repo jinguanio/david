7b4f6a6eeb027647b0aba1430b8af94daebf0e09
libo <libo@eyou.net>
2014-06-25 11:07:03 +0800

    增加 daemon 程序方便以后编写多进程 测试 redis 多进程获取数据是否存在重复

--- code/php/daemon/daemon.php
+++ code/php/daemon/daemon.php
@@ -1,111 +1,235 @@
 <?php
-// return child process id
-function create_daemon($daemon_file = null)
+error_reporting(E_ALL);
+
+declare(ticks = 1);
+
+/**
+ * Daemon 
+ * 
+ * @author David
+ *
+ * Example:
+ *   $daemon = new Daemon;
+ *   $daemon->set_options(
+ *      [
+ *          'func' => 'get_queue',
+ *          'fork_num' => 10,
+ *      ]
+ *   );
+ *   $daemon->fork_proc();
+ *
+ *   $daemon->kill_proc();
+ *
+ */
+class Daemon
 {
-    pcntl_signal(SIGTTOU, SIG_IGN);
-    pcntl_signal(SIGTTIN, SIG_IGN);
-    pcntl_signal(SIGTSTP, SIG_IGN);
-    pcntl_signal(SIGHUP, SIG_IGN);
+    // {{{ members
 
-    $pid = pcntl_fork();
-    if (0 > $pid) {
-        _log('fork child process fail', __LINE__);
-        exit(1);
-    } elseif ($pid) {
-        exit(0);
+    /**
+     * 子进程 id 
+     * 
+     * @var mixed
+     */
+    private $__pids = [];
+
+    /**
+     * 子进程执行行数 
+     * 
+     * @var string
+     */
+    private $__func = '';
+
+    /**
+     * 子进程个数 
+     * 
+     * @var float
+     */
+    private $__fork_num = 5;
+
+    /**
+     * pid 文件路径 
+     * 
+     * @var string
+     */
+    private $__pid_file = '/tmp/proc.pid';
+
+    // }}}
+    // {{{ functions
+    // {{{ public function __construct()
+
+    /**
+     * __construct 
+     * 
+     * @param array $args 
+     *  [
+     *      'func' => 'abc',
+     *      'fork_num' => 5,
+     *      'pid_file' => '/tmp/proc.pid',
+     *  ]
+     * @return void
+     */
+    public function __construct($args = null)
+    {
+        $this->set_options($args);
     }
 
-    if (0 > posix_setsid()) {
-        _log('set session leader fail', __LINE__);
-        exit(1);
-    }
+    // }}}
+    // {{{ public function set_options()
 
-    $pid = pcntl_fork();
-    if (-1 === $pid) {
-        _log('fork child process fail', __LINE__);
-        exit(1);
-    } elseif ($pid) {
-        return $pid;
-    }
-
-    if (!chdir('/tmp')) {
-        _log('child process set dir fail', __LINE__);
-        exit(1);
-    }
-
-    umask(0);
-
-    if (!pcntl_signal(SIGTERM, 'handler_sigterm')) {
-        _log('init_daemon, set signal handler for SIGTERM failed.', __LINE__);
-        exit(1);
-    }
-
-    if (!pcntl_signal(SIGCHLD, 'handler_sigchld')) {
-        _log('init_daemon, set signal handler for SIGCHLD failed.', __LINE__);
-        exit(1);
-    }
-
-    _log('init_daemon success.', __FILE__);
-    
-    pcntl_exec('/usr/local/eyou/mail/opt/bin/php ' . $daemon_file);
-    //exec('php ' . $daemon_file);
-    //while (1) {
-    //    sleep(3);
-    //}
-}
-
-function _log($msg, $line)
-{
-    $msg = date('r') . " [{$line}], {$msg}" . PHP_EOL;
-    file_put_contents('daemon.log', $msg, FILE_APPEND);
-}
-
-function clear_log()
-{
-    file_put_contents('daemon.log', '');
-}
-
-function handler_sigterm()
-{
-    _log('catch signal SIGTERM, exiting...', __LINE__);
-    if (!pcntl_signal(SIGCHLD, SIG_IGN)) {
-        _log('set signal handler for SIGTERM failed.', __LINE__);
-    }
-    if (!posix_kill(0, SIGTERM)) {
-        _log(posix_strerror(posix_get_last_error()), __LINE__);
-    }
-    while (($pid = pcntl_waitpid(-1, $status, WNOHANG)) > 0) {
-        if ($pid < 0) { // 无可回收的进程
-            break;
+    /**
+     * 设置配置参数 
+     * 
+     * @param array $args 
+     *  [
+     *      'func' => 'abc',
+     *      'fork_num' => 5,
+     *      'pid_file' => '/tmp/proc.pid',
+     *  ]
+     * @return void
+     */
+    public function set_options($args)
+    {
+        if (isset($args['func'])) {
+            $this->__func = $args['func'];
         }
-        _log("stop child $pid success.", __LINE__);
-    }
-    _free_parent(posix_getpid());
-    _log('exit by signal SIGTERM', __LINE__);
-    exit(0);
-}
 
-function handler_sigchld()
-{
-    _log('catch signal SIGCHLD', __LINE__);
-    while (($pid = pcntl_waitpid(-1, $status, WNOHANG)) > 0) {
-        if ($pid < 0) { // 无可回收的进程
-            break;
+        if (isset($args['fork_num'])) {
+            $this->__fork_num = $args['fork_num'];
         }
-        _log("child $pid exit, status: $status.", __LINE__);
-        _free_child($pid, $status);
-        usleep(100000);
+
+        if (isset($args['pid_file'])) {
+            $this->__pid_file = $args['pid_file'];
+        }
     }
-}
 
-function _free_parent()
-{
-}
+    // }}}
+    // {{{ public function set_option()
 
-function _free_child()
-{
-}
+    /**
+     * 设置配置参数 
+     * 
+     * @param string $name 
+     * @param mixed $val 
+     * @return void
+     */
+    public function set_option($name, $val)
+    {
+        $prop = '__' . $name;
+        if (property_exists($this, $prop)) {
+            $this->$prop = $val;
+        }
+    }
 
-clear_log();
-create_daemon('/tmp/test.php');
+    // }}}
+    // {{{ public function fork_proc()
+
+    /**
+     * 创建多进程 
+     * 
+     * @return void
+     */
+    public function fork_proc()
+    {
+        // 忽略终端 I/O信号,STOP信号
+        pcntl_signal(SIGTTOU, SIG_IGN);
+        pcntl_signal(SIGTTIN, SIG_IGN);
+        pcntl_signal(SIGTSTP, SIG_IGN);
+        pcntl_signal(SIGHUP, SIG_IGN);
+       
+        $pid = pcntl_fork();
+        if (0 != $pid) {
+            exit(0);
+        }
+
+        $this->_write_pid();
+        posix_setsid();
+        chdir('/tmp');
+        umask(0);
+        $this->_fork_child();
+
+        pcntl_signal(SIGUSR1, array($this, '_kill_child'));
+
+        while (1) {
+            sleep(60);
+        }
+    }
+
+    // }}}
+    // {{{ public function kill_proc()
+
+    /**
+     * 停止进程 
+     * 
+     * @return void
+     */
+    public function kill_proc()
+    {
+        $ppid = file_get_contents($this->__pid_file);
+        posix_kill($ppid, SIGUSR1);
+    }
+
+    // }}}
+    // {{{ private function _fork_child()
+
+    /**
+     * 创建子进程 
+     * 
+     * @param int $num 子进程个数
+     * @return void
+     */
+    private function _fork_child()
+    {
+        for ($i = 0; $i < $this->__fork_num; $i++) {
+            $pid = pcntl_fork();
+
+            pcntl_signal(SIGTERM, SIG_DFL);
+            pcntl_signal(SIGCHLD, SIG_DFL);
+
+            if ($pid == -1) {
+                die('could not fork');
+            } else if ($pid) {
+                $this->__pids[] = $pid;
+                pcntl_wait($status, WNOHANG); 
+            } else {
+                call_user_func($this->__func);
+            }
+        }
+    }
+
+    // }}}
+    // {{{ private function _kill_child()
+
+    /**
+     * 停止子进程 
+     * 
+     * @return void
+     */
+    private function _kill_child()
+    {
+        foreach ($this->__pids as $p) {
+            posix_kill($p, SIGTERM);
+        }
+
+        $ppid = file_get_contents($this->__pid_file);
+        posix_kill($ppid, SIGTERM);
+    }
+
+    // }}}
+    // {{{ private function _write_pid()
+
+    /**
+     * 写入 pid 文件 
+     * 
+     * @return void
+     */
+    private function _write_pid()
+    {
+        $fp = dio_open($this->__pid_file, O_WRONLY|O_CREAT, 0644);  
+        dio_truncate($fp, 0);
+        dio_write($fp, posix_getpid()); 
+    }
+
+    // }}}
+    // }}}
+}
 
--- code/php/esop/multiproc_redis.php
+++ code/php/esop/multiproc_redis.php
@@ -1,118 +1,8 @@
 <?php
 error_reporting(E_ALL);
+require_once __DIR__ . '/../daemon/daemon.php';
 
-declare(ticks = 1);
-
-$pids = [];
 $redis_key = 'libo-123';
-$pid_file = '/tmp/proc.pid';
-
-// {{{ function fork_proc()
-
-/**
- * fork_proc 
- * 
- * @param string $func 函数名
- * @param int $fork_num fork 进程数
- * @return void
- * @throws Exception
- */
-function fork_proc($func, $fork_num = 5)
-{
-    global $pids;
-
-    // 忽略终端 I/O信号,STOP信号
-    pcntl_signal(SIGTTOU, SIG_IGN);
-    pcntl_signal(SIGTTIN, SIG_IGN);
-    pcntl_signal(SIGTSTP, SIG_IGN);
-    pcntl_signal(SIGHUP, SIG_IGN);
-   
-    $pid = pcntl_fork();
-    if (0 != $pid) {
-        exit(0);
-    }
-
-    write_pid();
-    posix_setsid();
-    chdir('/tmp');
-    umask(0);
-    fork_child($func, $fork_num);
-
-    pcntl_signal(SIGUSR1, 'kill_child');
-
-    while (1) {
-        sleep(60);
-    }
-}
-
-// }}}
-// {{{ function fork_child()
-
-function fork_child($func, $num)
-{
-    global $pids;
-
-    for ($i = 0; $i < $num; $i++) {
-        $pid = pcntl_fork();
-
-        pcntl_signal(SIGTERM, SIG_DFL);
-        pcntl_signal(SIGCHLD, SIG_DFL);
-
-        if ($pid == -1) {
-            die('could not fork');
-        } else if ($pid) {
-            $pids[] = $pid;
-            pcntl_wait($status, WNOHANG); 
-        } else {
-            $func();
-        }
-    }
-}
-
-// }}}
-// {{{ function write_pid()
-
-function write_pid()
-{
-    global $pid_file;
-
-    $fp = dio_open($pid_file, O_WRONLY|O_CREAT, 0644);  
-    dio_truncate($fp, 0);
-    dio_write($fp, posix_getpid()); 
-}
-
-// }}}
-// {{{ function kill_proc()
-
-/**
- * kill_proc 
- * 
- * @return void
- * @throws Exception
- */
-function kill_proc()
-{
-    global $pid_file;
-    $ppid = file_get_contents($pid_file);
-    posix_kill($ppid, SIGUSR1);
-}
-
-// }}}
-// {{{ function kill_child()
-
-function kill_child()
-{
-    global $pids, $pid_file;
-
-    foreach ($pids as $p) {
-        posix_kill($p, SIGTERM);
-    }
-
-    $ppid = file_get_contents($pid_file);
-    posix_kill($ppid, SIGTERM);
-}
-
-// }}}
 
 // {{{ function set_queue()
 
@@ -126,7 +16,7 @@ function set_queue($total = 10000)
         $redis->rPush($redis_key, $i);
     }
 
-    lg();
+    //lg();
     echo "insert redis queue succ\n";
 }
 
@@ -142,7 +32,8 @@ function get_queue()
     while (1) {
         $ret = $redis->blPop($redis_key, 30);
         if (isset($ret[1])) {
-            lg('val: ' . var_export($ret[1], true));
+            //lg("[" . posix_getpid() . '] ' . var_export($ret[1], true));
+            lg($ret[1]);
         }
         usleep(5000);
     }
@@ -187,16 +78,22 @@ HELP;
 
 // }}}
 
+$daemon = new Daemon();
+$daemon->set_options([
+    'func' => 'get_queue',
+    'fork_num' => 10,
+    ]);
+
 if (isset($argv[1])) {
     if ('i' === $argv[1]) { // insert redis
         $t1 = microtime(true);
-        set_queue(1000000);
+        set_queue(10000);
         $t2 = microtime(true);
         echo 'eclipse: ' . ($t2-$t1) . "\n";
     } elseif ('r' === $argv[1]) { // run multi-process
-        fork_proc('get_queue', 100);
+        $daemon->fork_proc();
     } elseif ('k' === $argv[1]) { // kill multi-process
-        kill_proc();
+        $daemon->kill_proc();
     } else {
         help();
     }
