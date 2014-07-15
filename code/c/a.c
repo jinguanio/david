30a3d22b42c190328ed4b7f92cc711312b692970
libo <libo@eyou.net>
2014-07-15 19:00:21 +0800

    增加 csupport c 语言 vim 插件

---
+++ code/c/a.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a.c
+ *
+ *    Description:  David
+ *
+ *        Version:  1.0
+ *        Created:  2014年07月15日 14时18分57秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdio.h>
+
+int
+main ( int argc, char *argv[] )
+{
+    char str[] = "i am libo";
+    printf("%s\n", str);
+    return 0;
+}				/* ----------  end of function main  ---------- */
