490b9e843a979f827532691b787345be9c114ffc
libo <libo@eyou.net>
2015-05-06 19:02:29 +0800

    算法希尔排序、合并排序

---
+++ code/php/arithmetic/merge.php
@@ -0,0 +1,35 @@
+<?php
+class MergeSort2
+{
+    private function _merge($arr1, $arr2)
+    {
+        $arr3 = array();
+        while(!empty($arr1) && !empty($arr2)){
+            // 比较第一个元素，取较小的值
+            $arr3[] = $arr1[0] <= $arr2[0] ? array_shift($arr1) : array_shift($arr2);
+        }
+        // 剩下的$arr1和$arr2中都是较大的值，且他们是有序的，故可以直接合并到$arr3后面
+        $arr3 = array_merge($arr3, $arr1, $arr2);
+        return $arr3;
+    }
+
+    // 归并排序算法
+    public function msort($arr)
+    {
+        if(count($arr) <= 1) return $arr;
+    
+        // 将大数组拆分为两个数组
+        $mid = intval(count($arr)/2);
+        $arr1 = array_slice($arr, 0, $mid);
+        $arr2 = array_slice($arr, $mid);
+
+        // 利用递归思想，不断拆分数组，再逐步merge回来
+        return $this->_merge($this->msort($arr1), $this->msort($arr2));
+    }
+}
+
+$arr = [1,43,54,62,21,66,32,78,36,76,39];
+
+$ms2 = new MergeSort2;
+print_r($ms2->msort($arr));
+
---
+++ code/php/arithmetic/shell.php
@@ -0,0 +1,37 @@
+<?php
+$arr = [1,43,54,62,21,66,32,78,36,76,39];
+
+function shell($arr)
+{
+    $n = count($arr);
+    $h = 1;
+    while ($h < $n / 3) { 
+        $h = $h * 3 + 1;
+    }
+
+    while ($h >= 1) {
+        for ($i = 1; $i < $n; $i++) {
+            for ($j = $i; $j >= $h; $j = $j - $h) {
+                if ($arr[$j] < $arr[$j - $h]) {
+                    swap($arr, $j, $j - $h);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        $h = floor($h / 3);
+    }
+
+    return $arr;
+}
+
+function swap(&$arr, $i, $j)
+{
+    $tmp = $arr[$i];
+    $arr[$i] = $arr[$j];
+    $arr[$j] = $tmp;
+}
+
+print_r(shell($arr));
+
